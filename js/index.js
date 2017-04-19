37f1af0ee9e60aa50651efe344f716ee070eb133
Neal Lee <loseveny@qq.com>
2017-04-19 16:09:00 +1200

    the basic version of website. there are two issues need to be solved:   need to ask designer to provider exact font files.   need to ask design for currently working with area. I will fix that issues soon.

---
+++ js/index.js
@@ -0,0 +1,45 @@
+$(function(){
+
+	bindEvents();
+
+	function bindEvents(){
+
+		// set section's height
+		resizeHeight();
+		$(window).resize( resizeHeight );
+		function resizeHeight(){
+			$('.t-pc-section').css('height', $(window).height());
+		}
+		resizeHeight();
+
+		// $.scrollify({
+		// 	section:".t-pc-section",
+		// 	scrollbars: false,
+		// 	easing: "easeInOutQuart",
+		// 	before: function(){
+		// 		$('.currentimg').removeClass('currentimg');
+		// 	},
+		// 	after: function(){
+		// 		$('.t-pc-section .col-sm-6:not(.text) img').addClass('currentimg');
+		// 	}
+		// });
+
+		$(".t-top").on('click', function(event) {
+			if (this.hash !== "") {
+
+				event.preventDefault();
+
+				var hash = this.hash;
+
+				$('html, body').animate({
+					scrollTop: $(hash).offset().top
+				}, 900, function(){
+
+					window.location.hash = "home";
+				});
+			} // End if
+		});
+		
+	}
+});
+
